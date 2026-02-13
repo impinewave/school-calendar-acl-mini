aab1be0f740dddc175d4f4377eedf32218761b75
pinewave <pinewave.kr@gamil.com>
2026-02-13 09:16:01 +0900

    Add demo UI and README architecture section

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // server.js
+
 const express = require("express");
 const { initDb, run, get, all } = require("./db");
+const path = require("path");
 
 const app = express();
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
 /**
  * Fake auth via headers:
